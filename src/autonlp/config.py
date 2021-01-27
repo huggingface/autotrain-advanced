6727ef5fa2e8044fd07b7846d39956cfd5ba69af
SBrandeis <s.brandeis@orange.fr>
2021-01-27 13:49:17 +0100

    Make backend endpoint configurable

--- src/autonlp/config.py
+++ src/autonlp/config.py
@@ -1,1 +1,5 @@
-HF_AUTONLP_BACKEND_API = "http://autonlp-app-alpha-load-balancer-841182397.us-east-1.elb.amazonaws.com"
+import os
+
+HF_AUTONLP_BACKEND_API = os.getenv(
+    "HF_AUTONLP_BACKEND_API", "http://autonlp-app-alpha-load-balancer-841182397.us-east-1.elb.amazonaws.com"
+)
