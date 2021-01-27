2d08255700717c49fe002f29e10a5f1d5cba6948
Simon Brandeis <33657802+SBrandeis@users.noreply.github.com>
2021-01-27 13:30:48 +0100

    New project_info command (#4)

---
+++ src/autonlp/cli/project_info.py
@@ -0,0 +1,34 @@
+import sys
+from argparse import ArgumentParser
+
+from loguru import logger
+
+from ..tasks import TASKS
+from . import BaseAutoNLPCommand
+
+
+def create_project_command_factory(args):
+    return ProjectInfoCommand(args.name)
+
+
+class ProjectInfoCommand(BaseAutoNLPCommand):
+    @staticmethod
+    def register_subcommand(parser: ArgumentParser):
+        create_project_parser = parser.add_parser("project_info")
+        create_project_parser.add_argument("--name", type=str, default=None, required=True, help="Project Name")
+        create_project_parser.set_defaults(func=create_project_command_factory)
+
+    def __init__(self, name: str):
+        self._name = name
+
+    def run(self):
+        from ..autonlp import AutoNLP
+
+        logger.info(f"Fetching info for project: {self._name}")
+        client = AutoNLP()
+        try:
+            project = client.get_project(name=self._name)
+        except ValueError:
+            logger.error(f"Project {self._name} not found! You can create it using the create_project command.")
+            sys.exit(1)
+        print(project)
