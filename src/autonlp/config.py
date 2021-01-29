f1dcf4d91d1cb0cf93173c9a866914ab63458604
Abhishek Thakur <abhishek4@gmail.com>
2021-01-29 16:20:56 +0100

    update endpoint

--- src/autonlp/config.py
+++ src/autonlp/config.py
@@ -1,6 +1,4 @@
 import os
 
 
-HF_AUTONLP_BACKEND_API = os.getenv(
-    "HF_AUTONLP_BACKEND_API", "http://autonlp-app-alpha-load-balancer-841182397.us-east-1.elb.amazonaws.com"
-)
+HF_AUTONLP_BACKEND_API = os.getenv("HF_AUTONLP_BACKEND_API", "https://autonlp.huggingface.co")
