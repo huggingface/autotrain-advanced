5bb8c32667b325d8f267994f1701c0a0be0dc417
Simon Brandeis <33657802+SBrandeis@users.noreply.github.com>
2021-02-01 16:23:13 +0100

    Implement Auth in autonlp frontend (#5)

--- src/autonlp/cli/login.py
+++ src/autonlp/cli/login.py
@@ -1,29 +1,24 @@
 from argparse import ArgumentParser
 
-from loguru import logger
-
 from . import BaseAutoNLPCommand
 
 
 def login_command_factory(args):
-    return LoginCommand(username=args.username, api_key=args.api_key)
+    return LoginCommand(api_key=args.api_key)
 
 
 class LoginCommand(BaseAutoNLPCommand):
     @staticmethod
     def register_subcommand(parser: ArgumentParser):
         login_parser = parser.add_parser("login")
-        login_parser.add_argument("--username", type=str, default=None, required=True, help="Username")
         login_parser.add_argument("--api-key", type=str, required=True, help="API key")
         login_parser.set_defaults(func=login_command_factory)
 
-    def __init__(self, username: str, api_key: str):
-        self._username = username
+    def __init__(self, api_key: str):
         self._api_key = api_key
 
     def run(self):
         from ..autonlp import AutoNLP
 
-        logger.info(f"Logging in using username: {self._username}")
         client = AutoNLP()
-        client.login(username=self._username, token=self._api_key)
+        client.login(token=self._api_key)
--- src/autonlp/config.py
+++ src/autonlp/config.py
@@ -2,3 +2,5 @@ import os
 
 
 HF_AUTONLP_BACKEND_API = os.getenv("HF_AUTONLP_BACKEND_API", "https://autonlp.huggingface.co")
+
+HF_API = os.getenv("HF_API", "https://huggingface.co/api")
