c0ca870474b41485c2e8844e48af03f4beb09410
Abhishek Thakur <abhishek4@gmail.com>
2021-02-17 14:26:09 +0100

    bump version, push to pypi

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ EXTRAS_REQUIRE = {
 
 setup(
     name="autonlp",
-    version="0.0.1",
+    version="0.0.2",
     description=DOCLINES[0],
     long_description="\n".join(DOCLINES[2:]),
     author="HuggingFace Inc.",
