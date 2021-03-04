e074bd9a8a63bb82de1d394e05bdfd94874ec618
Abhishek Thakur <abhishek4@gmail.com>
2021-03-04 16:55:11 +0100

    improved metrics

--- src/autonlp/metrics.py
+++ src/autonlp/metrics.py
@@ -1,9 +1,9 @@
+import json
 from dataclasses import dataclass
 
-import requests
 from prettytable import PrettyTable
 
-from .utils import BOLD_TAG, RESET_TAG, http_get
+from .utils import BOLD_TAG, RESET_TAG, http_post
 
 
 @dataclass
@@ -22,34 +22,18 @@ class Metrics:
             username=username,
         )
 
-    def model_metrics(self, model_id):
-        json_resp = http_get(f"/models/{self.username}/{model_id}", token=self._token, suppress_logs=True).json()
-        eval_dicts = [ed for ed in json_resp if "eval_loss" in ed]
-        eval_dicts = sorted(eval_dicts, key=lambda k: k["eval_loss"])
-        if len(eval_dicts) > 0:
-            return eval_dicts[0]
-
     def print(self):
-        resp = http_get(path=f"/projects/{self.project_id}/jobs", token=self._token, suppress_logs=True)
-        json_jobs = resp.json()
-        printout = ["~" * 35, f"🚀 AutoNLP Model Metrics for project: {self.project_name}", "~" * 35, ""]
-        printout.append(f"💾 you have {len(json_jobs)} models in this project")
-        print("\n".join(printout))
-        print("")
-        best_loss = 99999999
-        best_model = None
-        print_logs = PrettyTable([f"📚 {BOLD_TAG}Model ID{RESET_TAG}", f"📊 {BOLD_TAG}Eval Loss{RESET_TAG}"])
-        for job in json_jobs:
-            model_id = job["id"]
-            try:
-                metrics = self.model_metrics(model_id=model_id)
-                eval_loss = metrics["eval_loss"]
-                if eval_loss < best_loss:
-                    best_loss = eval_loss
-                    best_model = model_id
-                print_logs.add_row([model_id, eval_loss])
-            except (requests.exceptions.HTTPError, TypeError):
-                continue
+        payload = {"project_id": self.project_id, "username": self.username}
+        resp = http_post(path="/models/metrics", token=self._token, suppress_logs=True, payload=payload)
+        json_jobs = json.loads(resp.json())
+        columns = json_jobs.keys()
+        print_logs = PrettyTable([col for col in columns])
+        print_logs.title = f"{BOLD_TAG}🚀 AutoNLP Model Metrics for project: {self.project_name}{RESET_TAG}"
+        indexes = json_jobs["model_id"].keys()
+        for idx in indexes:
+            line = [v[idx] if k == "model_id" else float(v[idx]) for k, v in json_jobs.items()]
+            print_logs.add_row(line)
+
+        print_logs = print_logs.get_string(sortby="eval_loss")
         print(print_logs)
         print("")
-        print(f"🎖 Best Model: {best_model}, Best Loss: {best_loss}")
