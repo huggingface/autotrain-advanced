81597d5d399c8cb71ebe04ea75444906a01e16f8
Abhishek Thakur <abhishek4@gmail.com>
2021-03-04 22:16:19 +0100

    update api & bump version

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ EXTRAS_REQUIRE = {
 
 setup(
     name="autonlp",
-    version="0.1.1",
+    version="0.1.2",
     description=DOCLINES[0],
     long_description="\n".join(DOCLINES[2:]),
     author="HuggingFace Inc.",
--- src/autonlp/config.py
+++ src/autonlp/config.py
@@ -1,6 +1,6 @@
 import os
 
 
-HF_AUTONLP_BACKEND_API = os.getenv("HF_AUTONLP_BACKEND_API", "https://autonlp.huggingface.co")
+HF_AUTONLP_BACKEND_API = os.getenv("HF_AUTONLP_BACKEND_API", "https://api.autonlp.huggingface.co")
 
 HF_API = os.getenv("HF_API", "https://huggingface.co/api")
