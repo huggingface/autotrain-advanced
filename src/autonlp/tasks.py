71fc1a5cf2b250daf4b8a15b7063d10a9a76b480
Abhishek Thakur <abhishek4@gmail.com>
2021-03-10 14:23:55 +0100

    Add support for single column regression

--- src/autonlp/tasks.py
+++ src/autonlp/tasks.py
@@ -1,1 +1,6 @@
-TASKS = {"binary_classification": 1, "multi_class_classification": 2, "entity_extraction": 4}
+TASKS = {
+    "binary_classification": 1,
+    "multi_class_classification": 2,
+    "entity_extraction": 4,
+    "single_column_regression": 10,
+}
