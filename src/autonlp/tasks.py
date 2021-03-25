4afc623d3c19046721cc11bc73896d8d1c14be8e
abhishek thakur <abhishekkrthakur@users.noreply.github.com>
2021-03-25 14:59:15 +0100

    add summarization task (#49)

--- src/autonlp/tasks.py
+++ src/autonlp/tasks.py
@@ -2,6 +2,7 @@ TASKS = {
     "binary_classification": 1,
     "multi_class_classification": 2,
     "entity_extraction": 4,
+    "summarization": 8,
     "single_column_regression": 10,
     "speech_recognition": 11,
 }
