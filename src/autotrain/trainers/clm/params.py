6bd970a3c2693e3c0dedf03c01867c82daf2bb57
Abhishek Thakur <abhishek4@gmail.com>
2023-10-12 15:04:06 +0200

    bump version

--- src/autotrain/trainers/clm/params.py
+++ src/autotrain/trainers/clm/params.py
@@ -46,7 +46,7 @@ class LLMTrainingParams(BaseModel):
     merge_adapter: bool = Field(False, title="Merge adapter")
     username: str = Field(None, title="Hugging Face Username")
     use_flash_attention_2: bool = Field(False, title="Use flash attention 2")
-    disable_gradient_checkpointing: bool = Field(True, title="Gradient checkpointing")
+    disable_gradient_checkpointing: bool = Field(False, title="Gradient checkpointing")
 
     def save(self, output_dir):
         os.makedirs(output_dir, exist_ok=True)
