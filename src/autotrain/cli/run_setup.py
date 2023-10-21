d5d6f30a9c1b0acc2962a4ed08f06110c75b8e55
Abhishek Thakur <abhishek4@gmail.com>
2023-10-21 12:32:50 +0200

    add colab arg

--- src/autotrain/cli/run_setup.py
+++ src/autotrain/cli/run_setup.py
@@ -7,7 +7,7 @@ from . import BaseAutoTrainCommand
 
 
 def run_app_command_factory(args):
-    return RunSetupCommand(args.update_torch)
+    return RunSetupCommand(args.update_torch, args.colab)
 
 
 class RunSetupCommand(BaseAutoTrainCommand):
@@ -22,10 +22,16 @@ class RunSetupCommand(BaseAutoTrainCommand):
             action="store_true",
             help="Update PyTorch to latest version",
         )
+        run_setup_parser.add_argument(
+            "--colab",
+            action="store_true",
+            help="Run setup for Google Colab",
+        )
         run_setup_parser.set_defaults(func=run_app_command_factory)
 
-    def __init__(self, update_torch: bool):
+    def __init__(self, update_torch: bool, colab: bool = False):
         self.update_torch = update_torch
+        self.colab = colab
 
     def run(self):
         # install latest transformers
@@ -53,7 +59,10 @@ class RunSetupCommand(BaseAutoTrainCommand):
         _, _ = pipe.communicate()
         logger.info("Successfully installed latest trl")
 
-        cmd = "pip install -U xformers"
+        if self.colab:
+            cmd = "pip install -U xformers"
+        else:
+            cmd = "pip uninstall -U xformers==0.0.22"
         pipe = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         logger.info("Installing latest xformers")
         _, _ = pipe.communicate()
