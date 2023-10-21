461186a6f230db3455c0cb802f0c905810db98b3
Abhishek Thakur <abhishek4@gmail.com>
2023-10-21 12:29:05 +0200

    bump version

--- src/autotrain/__init__.py
+++ src/autotrain/__init__.py
@@ -30,4 +30,4 @@ from autotrain.logging import custom_logger as logger
 warnings.filterwarnings("ignore", category=UserWarning, module="tensorflow")
 
 
-__version__ = "0.6.38.dev0"
+__version__ = "0.6.39.dev0"
