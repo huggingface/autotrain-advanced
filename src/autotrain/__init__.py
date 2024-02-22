390ac6a4b51b084ef7f671edc1388add0700d3d8
Abhishek Thakur <abhishek4@gmail.com>
2024-02-22 15:56:44 +0100

    peft + token ui fix

--- src/autotrain/__init__.py
+++ src/autotrain/__init__.py
@@ -24,10 +24,10 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
 import warnings
 
-from autotrain.logging import custom_logger as logger
+from autotrain.logging import custom_logger as logger  # noqa: F401
 
 
 warnings.filterwarnings("ignore", category=UserWarning, module="tensorflow")
 
 
-__version__ = "0.6.96.dev0"
+__version__ = "0.6.97.dev0"
