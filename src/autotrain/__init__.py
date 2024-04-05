8f3622bc4a1addd328ee37f4be5ca0073d7143d2
Abhishek Thakur <abhishek4@gmail.com>
2024-04-05 13:59:27 +0200

    bump version

--- src/autotrain/__init__.py
+++ src/autotrain/__init__.py
@@ -40,4 +40,4 @@ warnings.filterwarnings("ignore", category=UserWarning, module="datasets")
 warnings.filterwarnings("ignore", category=FutureWarning, module="accelerate")
 
 logger = Logger().get_logger()
-__version__ = "0.7.43.dev0"
+__version__ = "0.7.44.dev0"
