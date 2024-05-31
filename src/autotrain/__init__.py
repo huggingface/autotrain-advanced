7d6acc71301414c99beabbadce3850732b853adb
Abhishek Thakur <abhishek4@gmail.com>
2024-05-31 11:54:43 +0200

    bump version

--- src/autotrain/__init__.py
+++ src/autotrain/__init__.py
@@ -41,7 +41,7 @@ warnings.filterwarnings("ignore", category=FutureWarning, module="accelerate")
 warnings.filterwarnings("ignore", category=UserWarning, module="huggingface_hub")
 
 logger = Logger().get_logger()
-__version__ = "0.7.117.dev0"
+__version__ = "0.7.119.dev0"
 
 
 def is_colab():
