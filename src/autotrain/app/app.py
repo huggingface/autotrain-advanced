8e5cf613ef1b5999852639040b2dfb1c167adc7b
Abhishek Thakur <abhishek4@gmail.com>
2024-05-31 07:01:50 +0200

    fix query param

--- src/autotrain/app/app.py
+++ src/autotrain/app/app.py
@@ -1,6 +1,6 @@
 import os
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 
@@ -25,5 +25,9 @@ logger.info("AutoTrain started successfully")
 
 
 @app.get("/")
-async def forward_to_ui():
-    return RedirectResponse(url="/ui/")
+async def forward_to_ui(request: Request):
+    query_params = request.query_params
+    url = "/ui/"
+    if query_params:
+        url += f"?{query_params}"
+    return RedirectResponse(url=url)
