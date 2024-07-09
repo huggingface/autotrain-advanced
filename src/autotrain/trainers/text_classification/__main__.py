916b394f37c949ceda932c44b134da2fa3ef9981
Gaurav7888 <sarkargaurav7888@gmail.com>
2024-07-09 15:22:24 +0000

    adding code for gaudi config, gauditrainer

--- src/autotrain/trainers/text_classification/__main__.py
+++ src/autotrain/trainers/text_classification/__main__.py
@@ -4,6 +4,7 @@ import torch
 from accelerate.state import PartialState
 from datasets import load_dataset, load_from_disk
 from huggingface_hub import HfApi
+from dataclasses import dataclass, field
 from transformers import (
     AutoConfig,
     AutoModelForSequenceClassification,
@@ -31,6 +32,48 @@ from autotrain.trainers.text_classification.params import TextClassificationPara
 
 from optimum.habana import GaudiConfig, GaudiTrainer, GaudiTrainingArguments
 from optimum.habana.utils import set_seed
+from typing import Optional
+
+import logging
+import os
+import random
+import sys
+from dataclasses import dataclass, field
+from typing import Optional
+
+import datasets
+import evaluate
+import numpy as np
+import transformers
+from datasets import load_dataset
+from transformers import (
+    AutoConfig,
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    DataCollatorWithPadding,
+    EvalPrediction,
+    HfArgumentParser,
+    PretrainedConfig,
+    default_data_collator,
+)
+from transformers.trainer_utils import get_last_checkpoint
+from transformers.utils import check_min_version, send_example_telemetry
+from transformers.utils.versions import require_version
+
+from optimum.habana import GaudiConfig, GaudiTrainer, GaudiTrainingArguments
+from optimum.habana.utils import set_seed
+
+task_to_keys = {
+    "cola": ("sentence", None),
+    "mnli": ("premise", "hypothesis"),
+    "mrpc": ("sentence1", "sentence2"),
+    "qnli": ("question", "sentence"),
+    "qqp": ("question1", "question2"),
+    "rte": ("sentence1", "sentence2"),
+    "sst2": ("sentence", None),
+    "stsb": ("sentence1", "sentence2"),
+    "wnli": ("sentence1", "sentence2"),
+}
 
 def parse_args():
     # get training_config.json from the end user
@@ -38,9 +81,164 @@ def parse_args():
     parser.add_argument("--training_config", type=str, required=True)
     return parser.parse_args()
 
+@dataclass
+class DataTrainingArguments:
+    """
+    Arguments pertaining to what data we are going to input our model for training and eval.
+
+    Using `HfArgumentParser` we can turn this class
+    into argparse arguments to be able to specify them on
+    the command line.
+    """
+
+    task_name: Optional[str] = field(
+        default=None,
+        metadata={"help": "The name of the task to train on: " + ", ".join(task_to_keys.keys())},
+    )
+    dataset_name: Optional[str] = field(
+        default=None, metadata={"help": "The name of the dataset to use (via the datasets library)."}
+    )
+    dataset_config_name: Optional[str] = field(
+        default=None, metadata={"help": "The configuration name of the dataset to use (via the datasets library)."}
+    )
+    max_seq_length: int = field(
+        default=128,
+        metadata={
+            "help": (
+                "The maximum total input sequence length after tokenization. Sequences longer "
+                "than this will be truncated, sequences shorter will be padded."
+            )
+        },
+    )
+    overwrite_cache: bool = field(
+        default=False, metadata={"help": "Overwrite the cached preprocessed datasets or not."}
+    )
+    pad_to_max_length: bool = field(
+        default=True,
+        metadata={
+            "help": (
+                "Whether to pad all samples to `max_seq_length`. "
+                "If False, will pad the samples dynamically when batching to the maximum length in the batch."
+            )
+        },
+    )
+    max_train_samples: Optional[int] = field(
+        default=None,
+        metadata={
+            "help": (
+                "For debugging purposes or quicker training, truncate the number of training examples to this "
+                "value if set."
+            )
+        },
+    )
+    max_eval_samples: Optional[int] = field(
+        default=None,
+        metadata={
+            "help": (
+                "For debugging purposes or quicker training, truncate the number of evaluation examples to this "
+                "value if set."
+            )
+        },
+    )
+    max_predict_samples: Optional[int] = field(
+        default=None,
+        metadata={
+            "help": (
+                "For debugging purposes or quicker training, truncate the number of prediction examples to this "
+                "value if set."
+            )
+        },
+    )
+    train_file: Optional[str] = field(
+        default=None, metadata={"help": "A csv or a json file containing the training data."}
+    )
+    validation_file: Optional[str] = field(
+        default=None, metadata={"help": "A csv or a json file containing the validation data."}
+    )
+    problem_type: Optional[str] = field(
+        default="single_label_classification",
+        metadata={"help": "Problem type, such as single_label_classification or multi_label_classification"},
+    )
+    test_file: Optional[str] = field(default=None, metadata={"help": "A csv or a json file containing the test data."})
+
+    def __post_init__(self):
+        if self.task_name is not None:
+            self.task_name = self.task_name.lower()
+            if self.task_name not in task_to_keys.keys():
+                raise ValueError("Unknown task, you should pick one in " + ",".join(task_to_keys.keys()))
+        elif self.dataset_name is not None:
+            pass
+        elif self.train_file is None or self.validation_file is None:
+            raise ValueError("Need either a GLUE task, a training/validation file or a dataset name.")
+        else:
+            train_extension = self.train_file.split(".")[-1]
+            assert train_extension in ["csv", "json"], "`train_file` should be a csv or a json file."
+            validation_extension = self.validation_file.split(".")[-1]
+            assert (
+                validation_extension == train_extension
+            ), "`validation_file` should have the same extension (csv or json) as `train_file`."
+
+
+@dataclass
+class ModelArguments:
+    """
+    Arguments pertaining to which model/config/tokenizer we are going to fine-tune from.
+    """
+
+    model_name_or_path: str = field(
+        metadata={"help": "Path to pretrained model or model identifier from huggingface.co/models"}
+    )
+    config_name: Optional[str] = field(
+        default=None, metadata={"help": "Pretrained config name or path if not the same as model_name"}
+    )
+    tokenizer_name: Optional[str] = field(
+        default=None, metadata={"help": "Pretrained tokenizer name or path if not the same as model_name"}
+    )
+    cache_dir: Optional[str] = field(
+        default=None,
+        metadata={"help": "Where do you want to store the pretrained models downloaded from huggingface.co"},
+    )
+    use_fast_tokenizer: bool = field(
+        default=True,
+        metadata={"help": "Whether to use one of the fast tokenizer (backed by the tokenizers library) or not."},
+    )
+    model_revision: str = field(
+        default="main",
+        metadata={"help": "The specific model version to use (can be a branch name, tag name or commit id)."},
+    )
+    token: str = field(
+        default=None,
+        metadata={
+            "help": (
+                "The token to use as HTTP bearer authorization for remote files. If not specified, will use the token "
+                "generated when running `huggingface-cli login` (stored in `~/.huggingface`)."
+            )
+        },
+    )
+    trust_remote_code: bool = field(
+        default=False,
+        metadata={
+            "help": (
+                "Whether or not to allow for custom models defined on the Hub in their own modeling files. This option "
+                "should only be set to `True` for repositories you trust and in which you have read the code, as it will "
+                "execute code present on the Hub on your local machine."
+            )
+        },
+    )
+    ignore_mismatched_sizes: bool = field(
+        default=False,
+        metadata={"help": "Will enable to load a pretrained model whose head dimensions are different."},
+    )
+    add_pad_token: bool = field(
+        default=False,
+        metadata={"help": "Will add `pad_token` to tokenizer and model's config as `eos_token` if it's not defined."},
+    )
+
 
 # @monitor
 def train(config):
+    parser = HfArgumentParser((ModelArguments, DataTrainingArguments, GaudiTrainingArguments))
+    model_args, data_args, training_args = parser.parse_args_into_dataclasses()
     print(hthpu.is_available())
     device = torch.device("hpu")
     if isinstance(config, dict):
@@ -194,23 +392,31 @@ def train(config):
     # del training_args['output_dir']
     # del training_args['per_device_train_batch_size']
     # del training_args['per_device_eval_batch_size']
-    # gaudi_config = GaudiConfig.from_pretrained(
-    #     "Habana/bert-large-uncased-whole-word-masking",
-    #     cache_dir=None,
-    #     revision="main",
-    #     token=None,
-    # )
-    # trainer = GaudiTrainer(
-    #     gaudi_config=gaudi_config,
-    #     **training_args,
+    
+    # training_config.json add "Habana/bert-large-uncased-whole-word-masking",
+    gaudi_config = GaudiConfig.from_pretrained(
+        "Habana/bert-large-uncased-whole-word-masking",
+        cache_dir=None,
+        revision="main",
+        token=None,
+    )
+
+
+    trainer = GaudiTrainer(
+        model=model,
+        gaudi_config=gaudi_config,
+        args=training_args,
+        train_dataset=train_data,
+        eval_dataset=valid_data,
+        #compute_metrics=compute_metrics,
+        tokenizer=tokenizer,
+        #data_collator=data_collator,
+    )
+    # trainer = Trainer(
+    #     **trainer_args,
     #     train_dataset=train_data,
     #     eval_dataset=valid_data,
     # )
-    trainer = Trainer(
-        **trainer_args,
-        train_dataset=train_data,
-        eval_dataset=valid_data,
-    )
     trainer.remove_callback(PrinterCallback)
     trainer.train()
     print("Finished training, saving model...")
