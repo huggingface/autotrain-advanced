61c9cfd23e7110f53125e4739ede6baa3f06204a
abhishekkrthakur <abhishek4@gmail.com>
2024-11-08 10:36:06 +0100

    bump to dv

--- src/autotrain/__init__.py
+++ src/autotrain/__init__.py
@@ -45,7 +45,7 @@ warnings.filterwarnings("ignore", category=FutureWarning, module="accelerate")
 warnings.filterwarnings("ignore", category=UserWarning, module="huggingface_hub")
 
 logger = Logger().get_logger()
-__version__ = "0.8.27"
+__version__ = "0.8.28.dev0"
 
 
 def is_colab():
