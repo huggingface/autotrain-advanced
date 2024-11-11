f247ec75dbe3a5c4c3545befb993adcb0961fb17
abhishekkrthakur <abhishek4@gmail.com>
2024-11-11 15:54:53 +0100

    pip releaes

--- src/autotrain/__init__.py
+++ src/autotrain/__init__.py
@@ -45,7 +45,7 @@ warnings.filterwarnings("ignore", category=FutureWarning, module="accelerate")
 warnings.filterwarnings("ignore", category=UserWarning, module="huggingface_hub")
 
 logger = Logger().get_logger()
-__version__ = "0.8.31.dev0"
+__version__ = "0.8.31"
 
 
 def is_colab():
